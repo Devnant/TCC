9c5b2e644deaae7bf21820a62c2688ecd7014560
Devnant <robertocolares@hotmail.com>
2018-06-08 16:59:46 -0300

    Add files via upload

--- 3 - Consolidação.R
+++ 3 - Consolidação.R
@@ -17,11 +17,18 @@ base_5EF$desvidade<-(ifelse(base_5EF$TX_RESP_Q004=="A",-2,
 
 # Criar variável formpais
 base_5EF$formpais<-ifelse(base_5EF$TX_RESP_Q019=="F"|base_5EF$TX_RESP_Q023=="F","A","B")
+base_5EF$formpais<-factor(base_5EF$formpais,labels=c("Sim","Não"))
+base_5EF$formpais<-factor(base_5EF$formpais,levels(base_5EF$formpais)[c(2,1)])
 
 # Renomear variáveis
 colnames(base_5EF)[2]<-"sexo"
+base_5EF$sexo<-factor(base_5EF$sexo,labels=c("Masculino","Feminino"))
 colnames(base_5EF)[3]<-"percep"
+base_5EF$percep<-factor(base_5EF$percep,labels=c("Branco","Pardo","Preto","Amarelo","Indígena","Não Sei"))
+base_5EF$percep<-factor(base_5EF$percep,levels(base_5EF$percep)[c(3,1,2,4,5,6)])
 colnames(base_5EF)[7]<-"incentivo"
+base_5EF$incentivo<-factor(base_5EF$incentivo,labels=c("Sim","Não"))
+base_5EF$incentivo<-factor(base_5EF$incentivo,levels(base_5EF$incentivo)[c(2,1)])
 base_5EF<-base_5EF[c(-4:-6)]
 colnames(base_5EF)[5]<-"notalp"
 colnames(base_5EF)[6]<-"notamt"
@@ -42,11 +49,18 @@ base_9EF$desvidade<-(ifelse(base_9EF$TX_RESP_Q004=="A",2015-2001-14,
 
 # Criar variável formpais
 base_9EF$formpais<-ifelse(base_9EF$TX_RESP_Q019=="F"|base_9EF$TX_RESP_Q023=="F","A","B")
+base_9EF$formpais<-factor(base_9EF$formpais,labels=c("Sim","Não"))
+base_9EF$formpais<-factor(base_9EF$formpais,levels(base_9EF$formpais)[c(2,1)])
 
 # Renomear variáveis
 colnames(base_9EF)[2]<-"sexo"
+base_9EF$sexo<-factor(base_9EF$sexo,labels=c("Masculino","Feminino"))
 colnames(base_9EF)[3]<-"percep"
+base_9EF$percep<-factor(base_9EF$percep,labels=c("Branco","Pardo","Preto","Amarelo","Indígena","Não Sei"))
+base_9EF$percep<-factor(base_9EF$percep,levels(base_9EF$percep)[c(3,1,2,4,5,6)])
 colnames(base_9EF)[7]<-"incentivo"
+base_9EF$incentivo<-factor(base_9EF$incentivo,labels=c("Sim","Não"))
+base_9EF$incentivo<-factor(base_9EF$incentivo,levels(base_9EF$incentivo)[c(2,1)])
 base_9EF<-base_9EF[c(-4:-6)]
 colnames(base_9EF)[5]<-"notalp"
 colnames(base_9EF)[6]<-"notamt"
---
+++ Regressões.R
@@ -0,0 +1,81 @@
+load("./baseCONS.RData")
+
+paste(colnames(base_5EF),collapse="+")
+
+model1<- lme(notalp~1+sexo,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model1)
+
+model2<- lme(notalp~1+sexo+percep,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model2)
+
+anova(model1,model2)
+
+model3<- lme(notalp~1+sexo+percep+incentivo,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model3)
+
+anova(model2,model3)
+
+model4<- lme(notalp~1+sexo+percep+incentivo+desvidade,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model4)
+
+anova(model3,model4)
+
+model5<- lme(notalp~1+sexo+percep+incentivo+desvidade+formpais,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model5)
+
+anova(model4,model5)
+
+model6<- lme(notalp~1+sexo+percep+incentivo+desvidade+formpais+LOC,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model6)
+
+anova(model5,model6)
+
+model7<- lme(notalp~1+sexo+percep+incentivo+desvidade+formpais+LOC+DEPENDENCIA_ADM,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model7)
+
+anova(model6,model7)
+
+model8<- lme(notalp~1+sexo+percep+incentivo+desvidade+formpais+LOC+DEPENDENCIA_ADM+NIVELSOC,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model8)
+
+anova(model7,model8)
+
+model9<- lme(notalp~1+sexo+percep+incentivo+desvidade+formpais+LOC+DEPENDENCIA_ADM+NIVELSOC+VCONSERV,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model9)
+
+anova(model8,model9)
+
+model10<- lme(notalp~1+sexo+percep+incentivo+desvidade+formpais+LOC+DEPENDENCIA_ADM+NIVELSOC+VCONSERV,
+             data=base_5EF,
+             method="ML",
+             random=~1|ID_ESCOLA)
+summary(model10)
+
+anova(model9,model10)
